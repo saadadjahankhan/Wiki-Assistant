b2e7e0ba657ac9828b9a62b19b47c676f83f83b7
Mr.V3N0M <55073563+saadadjahankhan@users.noreply.github.com>
2019-11-15 12:25:47 +0600

    Create Wiki-Assitant-android.py

---
+++ Wiki-Assitant-android.py
@@ -0,0 +1,72 @@
+import wikipedia
+import tkinter as tk
+from tkinter import  *
+from tkinter import messagebox
+import webbrowser
+
+try:
+	import wikipedia
+	import tkinter as tk
+	from tkinter import  *
+	from tkinter import messagebox
+	import webbrowser
+except:
+	os.system("pip install tkinter")
+	os.system("pip install wikipedia")
+	os.system("pip install webbrowser")
+	os.system("clear")
+	
+def printresult():
+    re = wikipedia.summary(query.get())
+    text.delete(1.0, END)
+    text.insert(INSERT, re)
+
+def abt():
+	messagebox.showinfo("About","Wiki Assistant is a simple software which can scrap information about your search query from wikipedia.\nThis software was created by Saadad Jahan Khan [Mr.Venom]")
+
+def help():
+	messagebox.showinfo("Help", "Use the input box to enter your search query.\nAfter entering the query click on search button.Your result will be shown on the box.\n\nThanks for using this application.!")
+def contact():
+	webbrowser.open("https://facebook.com/ITXV3N0MDUD3")
+###gui
+root = tk.Tk()
+root.title("Wiki Assistant | Version 1.0")
+root.geometry("800x700")
+
+##text define
+text1 = Label(root,text="Wiki assistant",font=("tahoma",15,"underline"))
+text1.pack()
+
+##entrylabel
+label1 = Label(root,text="Enter text to search:  ",font=("tahoma",10,"bold"))
+label1.place(x=2,y=70)
+
+
+#entrybox
+query = Entry(root,width=70)
+query.place(x=230,y=71)
+
+#srchbtn
+button = Button(root,text="Search",command=printresult).place(x=610,y=101)
+
+###frame
+bottom=Frame(root,bg="silver")
+scrol=Scrollbar(bottom,background="grey")#414e4e
+scrol.pack(side=RIGHT,fill=Y)
+text=Text(bottom,wrap=WORD,yscrollcommand=scrol.set,background="white",undo=True)
+text.insert(INSERT,"Result will appear here!")
+text.pack()
+scrol.config(command=text.yview)
+bottom.place(x=80,y=200)
+
+##menubar
+menubar = Menu(root,bg="grey")
+root.config(menu=menubar)
+menubar.config(background="grey")
+menubar.add_command(label="About",command=abt)
+menubar.add_command(label="Help",command=help)
+menubar.add_command(label="Contact Us",command=contact)
+
+if __name__ == '__main__':
+	root.mainloop()
+
